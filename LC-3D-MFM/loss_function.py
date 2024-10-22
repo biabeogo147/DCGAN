b5d8cfb35c619bf4eef1933a643ebcdefcc8c343
biabeogo147 <biabeogo147@gmail.com>
2024-10-22 17:27:11 +0700

    lost function update:

--- LC-3D-MFM/loss_function.py
+++ LC-3D-MFM/loss_function.py
@@ -15,10 +15,52 @@ class LandMarkLoss(nn.Module):
         return loss
 
 
-# ----- Perceptual Loss using VGG16 -----
+class SegmentationLoss(nn.Module):
+    def __init__(self):
+        super(SegmentationLoss, self).__init__()
+        self.lip_mask_predictor = segmentation.deeplabv3_resnet101(pretrained=True).eval()
+
+    def forward(self, rendered_img, real_img):
+        rendered_img = rendered_img.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
+        real_img = real_img.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
+
+        mask1 = self.get_lip_mask(rendered_img)
+        mask2 = self.get_lip_mask(real_img)
+
+        distance_transform1 = cv2.distanceTransform(1 - mask1, cv2.DIST_L2, 5)
+        distance_transform2 = cv2.distanceTransform(1 - mask2, cv2.DIST_L2, 5)
+
+        contours1, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours2, _ = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        loss1_to_2 = self.calculate_loss_from_contours(contours1, distance_transform2)
+        loss2_to_1 = self.calculate_loss_from_contours(contours2, distance_transform1)
+        total_loss = loss1_to_2 + loss2_to_1
+
+        return total_loss
+
+    def get_lip_mask(self, img):
+        with torch.no_grad():
+            output = self.lip_mask_predictor(img)['out'][0]
+        output_predictions = output.argmax(0).byte().cpu().numpy()
+        lip_mask = (output_predictions == 15).astype(np.uint8)
+        return lip_mask
+
+    def calculate_loss_from_contours(self, contours, distance_transform):
+        total_points = 0
+        loss = 0
+        for contour in contours:
+            total_points += len(contour)
+            for point in contour:
+                x, y = point[0]
+                loss += distance_transform[int(y), int(x)]
+        return loss / total_points if total_points > 0 else 0
+
+
 class PerceptualLoss(nn.Module):
     def __init__(self):
         super(PerceptualLoss, self).__init__()
+        # Perceptual Loss using VGG16
         vgg = models.vgg16(pretrained=True).features
         self.slice = nn.Sequential(*list(vgg.children())[:16]).eval()
         for param in self.slice.parameters():
@@ -63,17 +105,15 @@ class FullLoss(nn.Module):
         self.lambda_dis = lambda_dis
 
         self.landmark_loss = LandMarkLoss()
+        self.segmentation_loss = SegmentationLoss()
         self.perceptual_loss = PerceptualLoss()
-        self.face_detector = dlib.get_frontal_face_detector()
-        self.landmark_predictor = dlib.shape_predictor(shape_predictor_path)
-        self.lip_mask_predictor = segmentation.deeplabv3_resnet101(pretrained=True).eval()
 
     def forward(self, rendered_img, real_img,
                 geometry_pred, geometry_graph,
                 expression_params):
         """ Normalize rendered_img before passing to FullLoss """
         Lland = self.landmark_loss(geometry_pred, geometry_graph)
-        Lseg = self.segmentation_consistency_loss(rendered_img, real_img)
+        Lseg = self.segmentation_loss(rendered_img, real_img)
         Lpho = self.photometric_consistency_loss(rendered_img, real_img)
         Lper = self.perceptual_loss(rendered_img, real_img)
         Lsmo = self.geometry_smoothness_loss(geometry_pred)
@@ -84,42 +124,6 @@ class FullLoss(nn.Module):
                       self.lambda_per * Lper +
                       self.lambda_smo * Lsmo +
                       self.lambda_dis * Ldis)
-        return total_loss
-
-    def segmentation_consistency_loss(self, rendered_img, real_img):
-        def get_lip_mask(img):
-            with torch.no_grad():
-                output = self.lip_mask_predictor(img)['out'][0]
-            output_predictions = output.argmax(0).byte().cpu().numpy()
-            lip_mask = (output_predictions == 15).astype(np.uint8)
-            return lip_mask
-
-        def calculate_loss_from_contours(contours, distance_transform):
-            total_points = 0
-            loss = 0
-            for contour in contours:
-                total_points += len(contour)
-                for point in contour:
-                    x, y = point[0]
-                    loss += distance_transform[int(y), int(x)]
-            return loss / total_points if total_points > 0 else 0
-
-        rendered_img = rendered_img.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
-        real_img = real_img.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
-
-        mask1 = get_lip_mask(rendered_img)
-        mask2 = get_lip_mask(real_img)
-
-        distance_transform1 = cv2.distanceTransform(1 - mask1, cv2.DIST_L2, 5)
-        distance_transform2 = cv2.distanceTransform(1 - mask2, cv2.DIST_L2, 5)
-
-        contours1, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours2, _ = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        loss1_to_2 = calculate_loss_from_contours(contours1, distance_transform2)
-        loss2_to_1 = calculate_loss_from_contours(contours2, distance_transform1)
-        total_loss = loss1_to_2 + loss2_to_1
-
         return total_loss
 
     def photometric_consistency_loss(self, rendered_img, real_img):
